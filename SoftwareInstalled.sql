02a7296e89ca3d4b2f423261685cc285b604c42f
Gabriel Anjos <42248998+gabrielofanjos@users.noreply.github.com>
2023-01-04 22:06:33 -0300

    Create SoftwareInstalled.sql

---
+++ SoftwareInstalled.sql
@@ -0,0 +1,1 @@
+select *  from  SMS_R_System inner join SMS_G_System_INSTALLED_SOFTWARE on SMS_G_System_INSTALLED_SOFTWARE.ResourceId = SMS_R_System.ResourceId where SMS_G_System_INSTALLED_SOFTWARE.ARPDisplayName = "Google Chrome"
