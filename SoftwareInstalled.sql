ec95d7c2201a44e45e8a9b160c3b0c9956ada166
Gabriel Anjos <42248998+gabrielofanjos@users.noreply.github.com>
2023-01-04 22:07:52 -0300

    Update SoftwareInstalled.sql

--- SoftwareInstalled.sql
+++ SoftwareInstalled.sql
@@ -1,1 +1,3 @@
-select *  from  SMS_R_System inner join SMS_G_System_INSTALLED_SOFTWARE on SMS_G_System_INSTALLED_SOFTWARE.ResourceId = SMS_R_System.ResourceId where SMS_G_System_INSTALLED_SOFTWARE.ARPDisplayName = "Google Chrome"
+/*Software are installed on environment*/ 
+select *  from  SMS_R_System inner join SMS_G_System_INSTALLED_SOFTWARE on SMS_G_System_INSTALLED_SOFTWARE.ResourceId = SMS_R_System.ResourceId 
+where SMS_G_System_INSTALLED_SOFTWARE.ARPDisplayName = "Software Name"
