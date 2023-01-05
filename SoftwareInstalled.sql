13a423867dbc0bba03b5a70132618e5f9e107225
Gabriel Anjos <42248998+gabrielofanjos@users.noreply.github.com>
2023-01-05 08:59:23 -0300

    Delete SoftwareInstalled.sql

--- SoftwareInstalled.sql
+++
@@ -1,3 +0,0 @@
-/*Software are installed on environment*/ 
-select *  from  SMS_R_System inner join SMS_G_System_INSTALLED_SOFTWARE on SMS_G_System_INSTALLED_SOFTWARE.ResourceId = SMS_R_System.ResourceId 
-where SMS_G_System_INSTALLED_SOFTWARE.ARPDisplayName = "Software Name"
