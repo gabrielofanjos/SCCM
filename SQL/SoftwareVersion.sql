d93d7ea554526f622867d99ceb65b3045dbd91e9
Gabriel Anjos <42248998+gabrielofanjos@users.noreply.github.com>
2023-01-05 08:55:41 -0300

    Update SoftwareVersion.sql

--- SQL/SoftwareVersion.sql
+++ SQL/SoftwareVersion.sql
@@ -1,1 +1,6 @@
-*/ asdsa/*
+/*WQL query Software versions on SCCM database*/
+
+select ProductName0, ProductVersion0, UninstallString0, count (*) as 'Total' from V_GS_INSTALLED_SOFTWARE
+where ProductName0 like 'Software Nme%'
+group by ProductName0, ProductVersion0, UnistallString0
+ 
